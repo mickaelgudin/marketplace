60e9af5b4306e79d91b72ffc7339ccefd7ccd690
Nellyuki <nelly.ung18@gmail.com>
2021-06-02 09:19:35 +0200

    link back end and front-end

--- app/Controllers/Panier.php
+++ app/Controllers/Panier.php
@@ -7,38 +7,33 @@ class Panier extends BaseController
 {
 	public function index()
 	{
-		return view('panier');
-	}
-	
-	public function panier()
-	{
 		//activation de redis
 		$this->cache = \Config\Services::cache();
 		//$this->cache->delete("d");
 
 		//mettre un tableau vide et remplir des que l'utilisateur ajoute un article depuis le catalogue
-		$myArray = array(
-			"prixT"=> "13€",
+		$data = array(
+			"data"=>array(
 			"article1" => array(
 				"nom" => "lunette",
-				"prix" => "3€",
+				"prix" => 3,
 				"quantite" => 3
 			),
 			"article2" => array(
 				"nom" => "montre",
-				"prix" => "10€",
+				"prix" => 10,
 				"quantite" => 1
-			)
+			))
 		);
 
-		
-		
-		//enregistrer dans la key : utilisateur courant ou session courante
-		$this->cache->save('user', $myArray, 300);
-		$data = $this->cache->get('user');
-		var_dump($data);
 
-		//$this->load->view('panier', $data);
-		
+
+		//$this->cache->save('user', $myArray, 300);
+		//$data = $this->cache->get('user');
+		echo view('panier', $data);
+
 	}
+
+	
+
 }
--- app/Views/panier.php
+++ app/Views/panier.php
@@ -1,4 +1,8 @@
 <?php include "menu.php"; ?>
+
+
+
+
 <div class="col-sm-12 col-md-10 col-md-offset-1">
     <table class="table table-hover">
         <thead>
@@ -11,38 +15,51 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td class="col-sm-8 col-md-6">
-                    <div class="media">
-                        <a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a>
-                        <div class="media-body">
-                            <h4 class="media-heading"><a href="#">Package</a></h4>
-                            <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
-                            <span>Status: </span><span class="text-warning"><strong>In Stock</strong></span>
-                        </div>
-                    </div>
-                </td>
-                <td class="col-sm-1 col-md-1" style="text-align: center">
-                    <input type="email" class="form-control" id="exampleInputEmail1" value="3">
-                </td>
-                <td class="col-sm-1 col-md-1 text-center"><strong>$9.99</strong></td>
-                <td class="col-sm-1 col-md-1 text-center"><strong>$99.99</strong></td>
-                <td class="col-sm-1 col-md-1">
-                    <button type="button" class="btn btn-danger">
-                        <span class="fa fa-remove"></span> Remove
-                    </button>
-                </td>
-            </tr>
+            <?php
+            if (!empty($data)) {
+                foreach ($data as $article) {
 
-            <td> </td>
-            <td> </td>
-            <td> </td>
-            <td>
-                <h5>Subtotal</h5>
-            </td>
-            <td class="text-right">
-                <h5><strong>$999.99</strong></h5>
-            </td>
+                    echo '  <tr>
+                                <td class="col-sm-8 col-md-6">
+                                    <div class="media">
+                                        <a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a>
+                                        <div class="media-body">
+                                            <h4 class="media-heading"><a href="#">';
+
+                    echo array_values($article)[0];
+
+                    echo '</a></h4>
+                                            <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
+                                            <span>Status: </span><span class="text-warning"><strong>In Stock</strong></span>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="col-sm-1 col-md-1" style="text-align: center">
+                                    <input type="number" class="form-control" value="';
+                    echo array_values($article)[2];
+                    echo '">
+                                </td>
+                                <td class="col-sm-1 col-md-1 text-center"><strong>';
+                    echo array_values($article)[1] . "€";
+                    echo '</strong></td>
+                                <td class="col-sm-1 col-md-1 text-center"><strong>';
+                    echo array_values($article)[1] * array_values($article)[2] . "€";
+                    echo '</strong></td>
+                                <td class="col-sm-1 col-md-1">
+                                    <button type="button" class="btn btn-danger">
+                                        <span class="fa fa-remove"></span> Remove
+                                    </button>
+                                </td>
+                            </tr>';
+                }
+            } else {
+                echo "panier vide";
+            }
+
+
+            ?>
+
+
             </tr>
             <td> </td>
             <td> </td>
@@ -51,7 +68,14 @@
                 <h3>Total</h3>
             </td>
             <td class="text-right">
-                <h3><strong>$9.999.99</strong></h3>
+                <h3><strong><?php
+                            $total = 0;
+                            foreach ($data as $article) {
+
+                                $total += array_values($article)[1] * array_values($article)[2];
+                            }
+
+                            echo $total . '€ </strong></h3>'; ?>
             </td>
             </tr>
             <tr>
